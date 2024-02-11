a68f4c2a057c45c2cb23252c479a9274ea85cd97
Abstract Intellects <AbstractIntellects@gmail.com>
2024-02-11 03:01:22 -0700

    Initial Commit

---
+++ Abstract Checkers macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Abstract Checkers macOS
+//
+//  Created by David Esparza on 2/11/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
